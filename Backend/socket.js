de717744a4823f7e590c03e01cb2b5e76dbe6f39
Md. Mazharul Islam <mazharul.saurav255@gmail.com>
2025-05-22 08:28:33 +0600

    one missing

---
+++ Backend/socket.js
@@ -0,0 +1,32 @@
+import { Server } from 'socket.io';
+
+let io;
+
+const initializeSocket = (server) => {
+    io = new Server(server, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST']
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log(`Client connected: ${socket.id}`);
+
+        socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${socket.id}`);
+        });
+    });
+};
+
+const sendMessageToSocketId = (socketId, messageObject) => {
+    console.log(messageObject);
+
+    if (io) {
+        io.to(socketId).emit(messageObject.event, messageObject.data);
+    } else {
+        console.log('Socket.io not initialized.');
+    }
+};
+
+export { initializeSocket, sendMessageToSocketId };
