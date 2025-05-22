798761d02d0247a586d687c649b866f9fb5649cc
Md. Mazharul Islam <mazharul.saurav255@gmail.com>
2025-05-22 09:24:02 +0600

    backend socket integration

--- Backend/socket.js
+++ Backend/socket.js
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io';
-
+import './db.js'
+import {pool} from "./db.js";
 let io;
 
 const initializeSocket = (server) => {
@@ -12,6 +13,34 @@ const initializeSocket = (server) => {
 
     io.on('connection', (socket) => {
         console.log(`Client connected: ${socket.id}`);
+
+        socket.on('join', async (data) => {
+            const { userId, userType } = data;
+
+            if (userType === 'user') {
+                try {
+                    const result = await pool.query(
+                        `UPDATE users SET socketid = $1 WHERE id = $2 RETURNING *`,
+                        [socket.id, userId]
+                    );
+                    console.log(`User updated: ${result.rows[0]}`);
+                } catch (error) {
+                    console.error('Error updating user socketid:', error);
+                }
+            }else if(userType === 'captain'){
+                try{
+                    const result = await pool.query(
+                        `UPDATE captaindetails SET socketid = $1 WHERE id = $2 RETURNING *`,
+                        [socket.id, userId]
+                    );
+                    console.log(`User updated: ${result.rows[0]}`);
+                }catch (error){
+                    console.error('Error updating captain socketid:', error);
+                }
+            }
+            console.log(`User joined: ${data}`);
+            socket.join(data);
+        });
 
         socket.on('disconnect', () => {
             console.log(`Client disconnected: ${socket.id}`);
